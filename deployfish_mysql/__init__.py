23dd4c384635a557ce7db3be5a8e9df03b12af6a
Chris Malek <cmalek@caltech.edu>
2022-08-30 15:47:08 -0700

    Bump version: 1.2.0 → 1.2.1

--- deployfish_mysql/__init__.py
+++ deployfish_mysql/__init__.py
@@ -5,7 +5,7 @@ from cement import App
 import deployfish_mysql.adapters  # noqa:F401
 from .controllers.mysql import MysqlController
 
-__version__ = "1.2.0"
+__version__ = "1.2.1"
 
 
 def add_template_dir(app: App):
