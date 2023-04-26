191c884d5ee2528c12a301a6bc58231ae407b3b7
Chris Malek <cmalek@caltech.edu>
2023-04-26 11:33:43 -0700

    FEATURE: the mysql: section of deployfish.yml can now support interpolations

--- deployfish_mysql/__init__.py
+++ deployfish_mysql/__init__.py
@@ -3,7 +3,10 @@ import os
 from cement import App
 
 import deployfish_mysql.adapters  # noqa:F401
+
 from .controllers.mysql import MysqlController
+from .hooks import pre_config_interpolate_add_mysql_section
+
 
 __version__ = "1.2.12"
 
@@ -16,3 +19,4 @@ def add_template_dir(app: App):
 def load(app: App) -> None:
     app.handler.register(MysqlController)
     app.hook.register('post_setup', add_template_dir)
+    app.hook.register('pre_config_interpolate', pre_config_interpolate_add_mysql_section)
